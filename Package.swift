b188a601f1d2111e87f1e76354acc4fbcd4942fe
Anatoly Myaskov <myaskov@me.com>
2018-06-20 17:36:21 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "libMemcachedSwift",
+    pkgConfig: "libmemcached",
+    providers: [
+        .brew(["libmemcached"]),
+        .apt(["libmemcached"])
+    ]
+)
